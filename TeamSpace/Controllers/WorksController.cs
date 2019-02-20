19b62abfe47f9431ac315d6da2f39b7a3fc8df18
vitaliyMarchenko <unityBars1>
2019-02-20 17:27:51 +0200

    Storing data in create task form implemented.

--- TeamSpace/Controllers/WorksController.cs
+++ TeamSpace/Controllers/WorksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using TeamSpace.Models;
 using TeamSpace.ViewModel;
 
@@ -10,14 +11,17 @@ namespace TeamSpace.Controllers
 {
     public class WorksController : Controller
     {
-        // GET: Works
+        // getting db context
         private readonly ApplicationDbContext _context;
 
         public WorksController()
         {
             _context = new ApplicationDbContext();
         }
-        // GET: Works
+        
+        // create task form
+        [Authorize]
+        [HttpGet]
         public ActionResult Create()
         {
             var viewModel = new WorkFormViewModel()
@@ -25,6 +29,27 @@ namespace TeamSpace.Controllers
                 TaskNames = _context.TaskNames.ToList()
             };
             return View(viewModel);
+        }
+        
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(WorkFormViewModel model)
+        {
+            var user = User.Identity.GetUserId();
+            var teamLead = _context.Users.Single(u => u.Id == user);
+            var taskName = _context.TaskNames.Single(t => t.Id == model.TaskName);
+            var work = new Work()
+            {
+                TeamLead = teamLead,
+                Location = model.Location,
+                DateTime = DateTime.Parse(string.Format("{0} {1}", model.Date, model.Time)),
+                TaskName = taskName
+            };
+
+            _context.Works.Add(work);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
